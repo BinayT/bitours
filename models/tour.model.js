89f1c74b3517dad56300ca98e871d81b8ffb8caf
Binay <binayprogramming@gmail.com>
2020-06-22 10:25:01 +0200

    added the dev data needed for the project in the actual project

--- models/tour.model.js
+++ models/tour.model.js
@@ -7,17 +7,50 @@ const tourSchema = new Schema({
     required: [true, 'Please indicate the name of the Tour'],
     unique: true,
   },
+  duration: {
+    type: Number,
+    required: [true, 'Please indicate the duration of the tour'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Please indicate the group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Please indicate the difficulty'],
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
     required: [true, 'Please indicate the price of the tour'],
   },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  difficulty: {
+  priceDiscount: Number,
+  summary: {
     type: String,
+    trim: true,
+    required: [true, 'Please indicate the summary'],
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Please indicate the image cover.'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
