0cdbfac28b51f19d8b849d0f2be9675639f881f0
Binay <binayprogramming@gmail.com>
2020-06-22 14:00:25 +0200

    Making the API better, added Advanced Filtering(gt,lt,gte,lte)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,12 +2,19 @@ const Tour = require('../models/tour.model');
 exports.getAllTours = async (req, res) => {
   try {
     //Build Query
-
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedField = ['page', 'sort', 'limit', 'fields'];
     excludedField.forEach((el) => delete queryObj[el]);
-    const query = Tour.find(queryObj);
 
+    //Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => {
+      return `$${match}`;
+    });
+
+    console.log(JSON.parse(queryStr));
+    const query = Tour.find(JSON.parse(queryStr));
     //Execute Query
 
     const tours = await query;
