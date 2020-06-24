645fc949a61524e92c211e5c6ec77e52d2bcfc1e
Binay <binayprogramming@gmail.com>
2020-06-24 10:54:53 +0200

    Refactored the API features, made utils files and added a class with all the API features so it's more reusable in the future

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tour.model');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.get5CheapTour = (req, res, next) => {
   req.query.limit = '5';
@@ -9,48 +10,14 @@ exports.get5CheapTour = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    //Build Query
-    // 1A) Filtering
-    const queryObj = { ...req.query };
-    const excludedField = ['page', 'sort', 'limit', 'fields'];
-    excludedField.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => {
-      return `$${match}`;
-    });
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Limiting Fields
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 3;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numOfTours = await Tour.countDocuments();
-      if (skip > numOfTours) throw new Error('This page does not exist.');
-    }
-
     //Execute Query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limit()
+      .pagination();
+
+    const tours = await features.query;
 
     //Send Response
     res.status(200).json({
