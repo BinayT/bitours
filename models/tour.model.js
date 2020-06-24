cf1ad7d2872dba1ff2de60d6efd4f871476f539d
Binay <binayprogramming@gmail.com>
2020-06-24 19:33:04 +0200

    Implemented Document Middleware of Mongoose

--- models/tour.model.js
+++ models/tour.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slugify = require('slugify');
 
 const tourSchema = new Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new Schema(
       required: [true, 'Please indicate the name of the Tour'],
       unique: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'Please indicate the duration of the tour'],
@@ -63,6 +65,22 @@ const tourSchema = new Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+//Document Middleware: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+/* tourSchema.pre('save', function (next) {
+  console.log("It's executed before post middleware!");
+  next();
+});
+
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
+}); */
 
 const Tour = mongoose.model('Tour', tourSchema);
 
